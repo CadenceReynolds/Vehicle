694bab4a547b088c8557cde4ce6129021bb9463c
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-09-12 13:57:15 -0400

    Finished(With some slight edits)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println("My cars make and model is a " + myCar.getMake() + ", " + myCar.getModel());
+
+        myCar.setMake("Chevy");
+        myCar.setModel("Hybrid");
+
+        System.out.println("My new cars make and model is a " + myCar.getMake() + ", " + myCar.getModel());
+
+
+
+    }
 }
